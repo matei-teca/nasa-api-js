622c68be70dc02a9dd404382ab2a53e6a0079154
Matei Teca <matei.teca@gmail.com>
2023-01-24 14:57:58 +0200

    adding necessary (all) files from More on favourite products

---
+++ client/public/script.js
@@ -0,0 +1,19 @@
+import { products } from '/data.js';
+
+
+const divElement = function (content){
+  return `<div>${content}</div>`;
+}
+
+const loadEvent = function() {
+
+  // Write your JavaScript code after this line
+
+
+  
+
+  // Write your JavaScript code before this line
+
+}
+
+window.addEventListener("load", loadEvent);
